352ce9779fd601545c906a6a9c9d1afa646214cc
Rafael-Endo <rcastrillonendo@gmail.com>
2025-09-09 15:39:23 -0500

    Add files via upload

---
+++ MatrizPx.h
@@ -0,0 +1,103 @@
+#pragma once
+#ifndef MATRIZPX_H
+#define MATRIZPX_H
+
+#include <vector>
+#include <random>
+#include <limits>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+
+
+struct MatrizPx {
+
+	int l;
+	int clase;
+	vector<string> pathImg;
+	const string PATH = "C:/proyectoPerceptron/main/main/img/";
+	const string TYPE = ".jpg";
+
+	MatrizPx(int _clase) {
+		clase = _clase;
+		if (clase == 1) l = 1;
+		else if (clase == 2) l = 7;
+		else if (clase == 3) l = 13;
+		else l = 19;
+		getPath();
+	}
+
+	void getPath() {
+		int cnt = l;
+		for (int i = l; i < l + 6; i++) {
+			string temp_path = PATH + to_string(i) + TYPE;
+			pathImg.push_back(temp_path);
+			cout << temp_path << endl;
+		}
+	}
+
+	void mostrarMatrizPx(const vector<vector<vector<vector<int>>>> matrizPxls) {
+		for (int idx = 0; idx < 6; idx++) {
+			if (matrizPxls[idx].empty()) {
+				cout << "Imagen vacia numero -> " << idx + 1 << endl;
+				continue;
+			}
+			cout << "Imagen numero -> " << idx + 1 << endl;
+			vector<vector<vector<int>>> tt = matrizPxls[idx];
+			cout << "[ ";
+			for (int i = 0; i < tt.size(); i++) {
+				for (int j = 0; j < tt[i].size(); j++) {
+					cout << " ( " << tt[i][j][0] << " , " << tt[i][j][1] << " , " << tt[i][j][2] << " , " << tt[i][j][3] << " ) " << ' ';
+				}
+				cout << endl;
+			}
+			cout << " ] " << endl;
+
+		}
+		cout << "clase -> " << clase << " cargada." << endl;
+	}
+
+	vector<vector<vector<vector<int>>>> getMatrizPx() {
+		vector<vector<vector<vector<int>>>> matrizPxls(6);
+		for (int idx = 0; idx < 6; idx++) {
+			cv::Mat img = cv::imread(pathImg[idx]);
+			if (img.empty()) {
+				cout << "Eror al leer la imagen -> " << pathImg[idx] << endl;
+			}
+			else {
+				for (int fila = 0; fila < img.rows; fila++) {
+					vector<vector<int>> filaPx(img.cols, vector<int>(4));
+					for (int columna = 0; columna < img.cols; columna++) {
+						cv::Vec3b pixelTemp = img.at<cv::Vec3b>(fila, columna);
+						int R = pixelTemp[0], G = pixelTemp[1], B = pixelTemp[2];
+						filaPx[columna][0] = B;filaPx[columna][1] = G;
+						filaPx[columna][2] = R;filaPx[columna][3] = clase;
+					}
+					matrizPxls[idx].push_back(filaPx);
+				}
+			}
+		}
+		if (matrizPxls.empty()) {
+			cout << "Error al procesar imagenes -> MatrizPx.h -> getMatrizPx()" << endl;
+			return { {{{}}} };
+		}
+		else {
+			cout << matrizPxls.size() << " imagenes procesadas -> MatrizPxl.j -> getMatrizPx()" << endl;
+			//rmostrarMatrizPx(matrizPxls);
+			return matrizPxls;
+		}
+		
+	}
+
+
+};
+
+
+
+
+
+#endif
---
+++ Perceptron.h
@@ -0,0 +1,125 @@
+#pragma once
+#ifndef PERCEPTRON_H
+#define PERCEPTRON_H
+
+
+#include <vector>
+#include <random>
+#include <limits>
+
+using namespace std;
+
+
+struct Color {
+    double sesgo, R, G, B;
+    int tipo;
+
+    Color(double sesgo1, double R1, double G1, double B1, int tipo1) {
+        sesgo = sesgo1;
+        R = R1;
+        G = G1;
+        B = B1;
+        tipo = tipo1;
+    }
+};
+
+struct Perceptron {
+
+    vector<Color> datos_colores;
+    vector<double> W;
+    double tasa_aprendizaje;
+    int claseColorG;
+
+    Perceptron(vector<vector<vector<vector<vector<int>>>>>& datos, double tasa_aprendizaje1, int _clase) {
+        claseColorG = _clase;
+        datos_colores = normalizar_colores(datos);
+        tasa_aprendizaje = tasa_aprendizaje1;
+        W.resize(4);
+        mt19937 rng(42);
+        normal_distribution<double> gen(0.0, 0.01);
+        for (int i = 0; i < 4; i++) {
+            double val = gen(rng);
+            W[i] = val;
+        }
+    }
+
+    void show() {
+        cout << "size ->" << datos_colores.size() << endl;
+        for (auto& _c : datos_colores) {
+            cout << "clase -> " << _c.tipo << endl;
+        }
+    }
+
+    vector<Color> normalizar_colores(const vector<vector<vector<vector<vector<int>>>>>& datos) {
+        const double norm = 255 * 1.0;
+        for (int clase = 0; clase < 4; clase++) {
+            for (int img = 0; img < 6; img++) {
+                vector<vector<vector<int>>> imgTemp = datos[clase][img];
+                for (int fila = 0; fila < imgTemp.size(); fila++) {
+                    for (int col = 0; col < imgTemp[fila].size(); col++) {
+                        double R = imgTemp[fila][col][0] * 1.0, G = imgTemp[fila][col][1] * 1.0, B = imgTemp[fila][col][2] * 1.0, sesgo = 1.0;
+                        int tipo = (imgTemp[fila][col][3] == claseColorG ? 1 : 0);
+                        R /= norm; G /= norm; B /= norm;
+                        Color pixel = { sesgo, R, G, B, tipo};
+                        datos_colores.push_back(pixel);
+                    }
+                }
+            }
+        }
+        //show();
+        if (datos_colores.empty()) {
+            cout << "No se encontraron datos -> perceptron -> nomralizar_colores.h" << endl;
+            return {};
+        }
+        cout << "datos en vector<Color> -> perceptron.h" << endl;
+        return datos_colores;
+    }
+
+    int producto_punto(const Color& c) const {
+        double res = (c.sesgo * W[0]) + (c.R * W[1]) + (c.G * W[2]) + (c.B * W[3]);
+        return (res >= 0.0 ? 1 : 0);
+    }
+
+    bool predecir(int R, int G, int B) {
+        double cast_R = (R * 1.0) / 255.0, cast_G = (G * 1.0) / 255.0, cast_B = (B * 1.0) / 255.0;
+        double res = (1.0 * W[0]) + (W[1] * cast_R) + (W[2] * cast_G) + (W[3] * cast_B);
+        return (res >= 0.0 ? 1 : 0);
+    }
+
+    void entrenar(int k, double eps) {
+        double error = numeric_limits<double>::max();
+        int epoca = 0;
+        while (error > eps && epoca < k) {
+            bool corre = 0;
+            for (int i = 0; i < datos_colores.size(); i++) {
+                const Color color_actual = datos_colores[i];
+                int predecir = producto_punto(color_actual);
+                int clase_real = color_actual.tipo;
+                int error_i = clase_real - predecir;
+                if (error_i != 0) {
+                    corre = 1;
+                    W[0] += tasa_aprendizaje * error_i * color_actual.sesgo;
+                    W[1] += tasa_aprendizaje * error_i * color_actual.R;
+                    W[2] += tasa_aprendizaje * error_i * color_actual.G;
+                    W[3] += tasa_aprendizaje * error_i * color_actual.B;
+                }
+            }
+            double error_temp = 0.0;
+            for (int i = 0; i < datos_colores.size(); i++) {
+                Color colo_actual = datos_colores[i];
+                int predecir = producto_punto(colo_actual);
+                error_temp += ((colo_actual.tipo * 1.0) - (predecir * 1.0)) * ((colo_actual.tipo * 1.0) - (predecir * 1.0));
+            }
+            error_temp = error_temp * 0.5;
+            error = error_temp;
+            epoca++;
+            if (corre == 0) {
+                break;
+            }
+        }
+    }
+
+};
+
+#endif
+
