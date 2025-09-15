b59903b203710c3584c0d050d480ee2aed75b892
Rafael-Endo <rcastrillonendo@gmail.com>
2025-09-14 20:47:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <utility>
 #include <functional>
@@ -8,15 +7,30 @@
 #include "MatrizPx.h"
 #include "InputImg.h"
 #include "JsonGF.h"
+#include "HttpRequest.h"
 #include <map>
+#include <chrono>
+#include <thread>
 #include <opencv2/opencv.hpp>
-
 
 using namespace std;
 
 vector<vector<vector<vector<vector<int>>>>> datos(4);
 const string INPUTIMG = "C:/proyectoPerceptron/InputImg/inputimg.jpg";
 const string JSONPATH = "C:/proyectoPerceptron/jsongf/jsongf.json";
+
+void selColor(string &col1, string& col2, string& col3) {
+	col1, col2, col3;
+	cout << "Color 1 (Clase 1) : ";
+	cin >> col1;
+	cout << endl;
+	cout << "Color 2 (Clase 2) : ";
+	cin >> col2;
+	cout << endl;
+	cout << "Color 3 (Clase 3) : ";
+	cin >> col3;
+	cout << endl;
+}
 
 void uploadDatos() {
 	for (int clase = 0; clase < 4; clase++) {
@@ -57,6 +71,7 @@ void mostrarDatos() {
 }
 
 map<int, string> res;
+map<string, int> resCl;
 Perceptron C1, C2, C3, EX;
 
 void init(const string &colorC1, const string &colorC2, const string &colorC3) {
@@ -65,20 +80,23 @@ void init(const string &colorC1, const string &colorC2, const string &colorC3) {
 	const int K = 1000;
 	const double err = 0.0;
 	res[1] = colorC1; res[2] = colorC2; res[3] = colorC3;
+	res[4] = "null";
+	resCl[colorC1] = 1; resCl[colorC2] = 2; resCl[colorC3] = 3;
+	resCl[res[4]] = 4;
 	C1 = Perceptron(datos, (double)0.01, (int)1); C2 = Perceptron(datos, (double)0.01, (int)2);
 	C3 = Perceptron(datos, (double)0.01, (int)3); EX = Perceptron(datos, (double)0.01, (int)4);
 	C1.entrenar(K, err); C2.entrenar(K, err);
 	C3.entrenar(K, err); EX.entrenar(K, err);
+	JsonGF wrgf(JSONPATH, C1.getWT(), C2.getWT(), C3.getWT(), C1.getET(), C2.getET(), C3.getET());
 }
 
 void showPre(const vector<pair<double, int>> cal) {
 	for (auto& x : cal) {
 		cout << "Color : " << res[x.second] << " | % = " << x.first << endl;
 	}
-
 }
 
-string predecirInput(const vector<vector<int>> &input) {
+string predecirInput(const vector<vector<int>>& input) {
 	int pxVal = 0;
 	double cnt1 = 0, cnt2 = 0, cnt3 = 0;
 	for (int px = 0; px < input.size(); px++) {
@@ -92,26 +110,49 @@ string predecirInput(const vector<vector<int>> &input) {
 		pxVal++;
 	}
 	if (pxVal == 0) {
-		return "ningun pixel valido";
+		return "null";
 	}
 	vector<pair<double, int>> cal(3);
 	cal[0] = { cnt1, 1 };
 	cal[1] = { cnt2, 2 };
 	cal[2] = { cnt3, 3 };
-	sort(cal.begin(), cal.end(), greater<pair<double,int>>());
+	sort(cal.begin(), cal.end(), greater<pair<double, int>>());
+	if (cal[0].first < 3.0) {
+		return "null";
+	}
+	cout << "cal -> " << cal[0].first << ' ';
 	return res[cal[0].second];
 }
 
+
+
+const int ms = 250;
+const string dirHttp = "http://10.250.243.227/capture/download";
+const string dwnlImg = "C:/proyectoPerceptron/InputImg/inputimg.jpg";
+
 int main()
 {
-	vector<vector<double>> W = { {0, 1, 2, 3}, {4, 5, 6,7} };
-	vector<double> E = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 };
-	JsonGF writeGF(JSONPATH, W, W, W, E, E, E);
-	/*
-	init("VERDE", "ROJO", "AMARILLO");
-	InputImg IN(INPUTIMG);
-	vector<vector<int>> input = IN.getMatrizInput();
-	cout <<" Pre -> " << predecirInput(input) << endl;
-	return 0;
-	 */
+	string col1, col2, col3;
+	selColor(col1, col2, col3);
+	init(col1, col2, col3);
+	cout << "paso init" << endl;
+	int _10 = 100;
+	while (true) {
+		bool rqst = HttpRequest::download(dirHttp, dwnlImg);
+		if (!rqst) {
+			cout << "Fallo en httprequest" << endl;
+			break;
+		}
+		InputImg in(INPUTIMG);
+		vector<vector<int>> input = in.getMatrizInput();
+		string prdc = predecirInput(input);
+		int idx = resCl[prdc];
+		if (idx == 4) {
+			cout << "Color invalido" << endl;
+		}
+		else {
+			cout << " Color -> " << prdc << endl;
+			JsonGF send(JSONPATH, idx);
+		}
+	}
 }
